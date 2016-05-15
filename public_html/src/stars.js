a1af294be1d2f57e84aaab2a0d315690a2a7ba5f
Kristian Pulkkinen <kristian.pulkkinen@cs.helsinki.fi>
2016-05-15 19:51:11 +0300

    Geometry lines

--- public_html/src/stars.js
+++ public_html/src/stars.js
@@ -4,73 +4,107 @@ var canvasHeight = null;
 var sectionBasePositions = null;
 
 var size = 156;
-var tilt = 28;
+var tilt = 27;
 
 var aisleAmount = 5;
 
-var track = 500;
+var track = 0;
+var defaultColor = "#000000";
+
+
+
+var star = {
+	x: 500,
+	y: 240,
+	size: 156,
+	getCenterPosition: function () {
+		return {x: star.x, y: star.y};
+	},
+	drawStuff: function () {
+		drawLogo();
+		drawDotLines();
+		drawSectionBasePositionts();
+		drawCenterCircle();
+
+		drawLineFromCentreToAisle(0);
+		drawLineFromCentreToAisle(1);
+
+		drawL(0);
+		d();
+	}
+};
+
+
+
+function d() {
+	var a = sectionBasePositions[0];
+	var b = sectionBasePositions[1];
+
+	var x = Math.abs(a.x - b.x);
+	var y = Math.abs(a.y - b.y);
+	var l = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	// console.log(x);
+
+	var posX = x / 2 + star.x;
+	var posY = y / 2 + sectionBasePositions[0].y;
+	var pos = {x: posX, y: posY};
+	drawCircle(posX, posY, 3);
+
+	drawLine(a, b, "yellow");
+
+	drawLine(pos, star.getCenterPosition(), "blue");
+}
+
+
+
+function drawL(i) {
+	i %= aisleAmount;
+	var pos = sectionBasePositions[i];
+
+	var degrees = 360 / aisleAmount;
+	var length = Math.cos(toRadians(72 - 90)) * star.size;
+
+	var x = pos.x + length * Math.cos(toRadians(degrees));
+	var y = pos.y + length * Math.sin(toRadians(degrees));
+
+	var p = {x: x, y: y};
+	drawLine(pos, p, "#FF9999");
+}
+
+
+function initTrack() {
+	track = 400;
+}
 
 
 function initSectionBasePositions() {
 	sectionBasePositions = {};
-	for (i = 0; i < aisleAmount; ++i) {
-		degrees = (360 * i / aisleAmount) % 360;
-		x = canvasWidth / 2 + size * Math.cos(toRadians(degrees - 90));
-		y = canvasHeight / 2 + size * Math.sin(toRadians(degrees - 90));
+	for (var i = 0; i < aisleAmount; ++i) {
+		var degrees = (360 * i / aisleAmount) % 360;
+		x = star.x + star.size * Math.cos(toRadians(degrees - 90));
+		y = star.y + star.size * Math.sin(toRadians(degrees - 90));
 		sectionBasePositions[i] = {x: x, y: y};
 	}
 }
 
 
-
-
 function calculateCoordinates(distance) {
-	transition = track / aisleAmount;
-	section = parseInt(distance / transition);
-	pos = sectionBasePositions[section];
-	distanceFromPos = distance % transition;
-	degrees = (360 * section / aisleAmount) % 360;
-	
-	x = pos.x + distanceFromPos * -Math.cos(toRadians(degrees - 90 - tilt));
-	y = pos.y + distanceFromPos * -Math.sin(toRadians(degrees - 90 - tilt));
+	var transition = track / aisleAmount;
+	var section = parseInt(distance / transition);
+	var pos = sectionBasePositions[section];
+	var distanceFromPos = distance % transition;
+	var degrees = (360 * section / aisleAmount) % 360;
+	var correction = 90;
+
+	var x = pos.x + distanceFromPos * -Math.cos(toRadians(degrees - correction - tilt));
+	var y = pos.y + distanceFromPos * -Math.sin(toRadians(degrees - correction - tilt));
 	return {x: x, y: y};
-}
-
-
-function init() {
-	canvas = $("canvas")[0];
-	ctx = $("canvas")[0].getContext("2d");
-	canvasWidth = canvas.width;
-	canvasHeight = canvas.height;
-	initSectionBasePositions();
-}
-
-
-function drawLogo() {
-	var img = $("#logo")[0];
-	dimension = 400;
-	x = canvasWidth / 2 - dimension / 2;
-	y = canvasHeight / 2 - dimension / 2;
-	ctx.drawImage(img, x, y, dimension, dimension);
-}
-
-
-
-function drawCircle(distance, radius) {
-	position = calculateCoordinates(distance);
-//	ctx.beginPath();
-//	ctx.arc(90, 55, 30, 0, 2 * Math.PI);
-//	ctx.stroke();
-	ctx.beginPath();
-	ctx.arc(position.x, position.y, radius, 0, 2*Math.PI);
-	ctx.stroke();
 }
 
 
 $(document).ready(function () {
 	init();
 	drawLogo();
-	
 	motor.init();
 });
 
@@ -78,30 +112,34 @@ $(document).ready(function () {
 
 function drawDotLines() {
 	for (i = 0; i < track; ++i) {
-		radius = 0.001;
-//		if (i % 19 === 0) {radius = 5;}
-		drawCircle(i + 0.001, radius);
+		var radius = 0.001;
+		drawCircleOnPath(i + 0.001, radius);
 	}
 }
 
 
+function drawSectionBasePositionts() {
+	for (i in sectionBasePositions) {
+		var pos = sectionBasePositions[i];
+		drawCircle(pos.x, pos.y, 5);
+	}
+}
+
 
 motor = {
 	tickCount: 0,
-	
-	init: function() {
+	init: function () {
 		motor.tick();
 	},
-	logic: function() {
-		ball.move(3);
+	logic: function () {
+		ball.move(0.5);
 	},
-	render: function() {
+	render: function () {
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-		drawLogo();
-		drawDotLines();
+		star.drawStuff();
 		ball.draw();
 	},
-	tick: function() {
+	tick: function () {
 		++motor.tickCount;
 		motor.logic();
 		motor.render();
@@ -111,16 +149,83 @@ motor = {
 
 
 
+
+
+
+
+function init() {
+	var canvas = $("canvas")[0];
+	ctx = $("canvas")[0].getContext("2d");
+	canvasWidth = canvas.width;
+	canvasHeight = canvas.height;
+	initTrack();
+	initSectionBasePositions();
+}
+
+
+function drawLogo() {
+	var img = $("#logo")[0];
+	var dimension = size * 2.72;
+	var x = star.x - dimension / 2;
+	var y = star.y - dimension / 2;
+	ctx.drawImage(img, x, y, dimension, dimension);
+}
+
+
+function drawCenterCircle() {
+	drawCircle(star.x, star.y, 1);
+	drawCircle(star.x, star.y, 10);
+}
+
+
+
+function drawCircleOnPath(distance, radius) {
+	var position = calculateCoordinates(distance);
+	drawCircle(position.x, position.y, radius);
+}
+
+
+function drawCircle(x, y, radius, color) {
+	ctx.strokeStyle = color || defaultColor;
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, 2 * Math.PI);
+	ctx.stroke();
+}
+
+
+function drawLineFromCentreToAisle(i) {
+	i %= aisleAmount;
+	drawLine(star.getCenterPosition(), sectionBasePositions[i], "#FF0000");
+
+}
+
+
+function drawLine(from, to, color) {
+	ctx.strokeStyle = color || defaultColor;
+	ctx.beginPath();
+	ctx.moveTo(from.x, from.y);
+	ctx.lineTo(to.x, to.y);
+	ctx.stroke();
+}
+
+
+
+
+
+
+
+
+
+
 var ball = {
 	position: 0,
 	radius: 3,
-	
-	move: function(speed) {
+	move: function (speed) {
 		ball.position += speed;
 		ball.position %= track;
 	},
-	draw: function() {
-		drawCircle(ball.position, ball.radius);
+	draw: function () {
+		drawCircleOnPath(ball.position, ball.radius);
 	}
 };
 
@@ -128,50 +233,25 @@ var ball = {
 
 
 
-toDegree = function(radians) {
-  return radians * (180 / Math.PI);
+toDegree = function (radians) {
+	return radians * (180 / Math.PI);
 };
 
-toRadians = function(degrees) {
-  return degrees * (Math.PI / 180);
+toRadians = function (degrees) {
+	return degrees * (Math.PI / 180);
 };
 
 
 
 
 window.requestAnimFrame = (function () {
-	return	window.requestAnimationFrame				|| 
-					window.webkitRequestAnimationFrame	|| 
-					window.mozRequestAnimationFrame			|| 
-					window.oRequestAnimationFrame				|| 
-					window.msRequestAnimationFrame			||
-					function(callback) {
-						window.setTimeout(callback, 1000/60);
-					};
-					}
-)();
-
-
-
-function testi() {
-	ctx.beginPath();
-
-	x1 = 400;
-	y1 = 100;
-
-	x2 = x1 + 30;
-	y2 = y1;
-
-	cp1x = 410;
-	cp1y = 60;
-
-	cp2x = x2 - (cp1x - x1);
-	cp2y = cp1y;
-
-	ctx.moveTo(x1, y1);
-	ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x2, y2);
-
-	ctx.lineWidth = 4;
-	ctx.strokeStyle = 'blue';
-	ctx.stroke();
+	return	window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function (callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
 }
+)();
